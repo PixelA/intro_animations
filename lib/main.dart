3c98e666174c018a8e506ace4feeb19420f65518
PixelA <asherleelevy@yahoo.com>
2019-04-24 02:11:36 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.amber
+      ),
+
+
+      home: MyHomePage(title: 'Animations in Flutter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  AnimationController controller;
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      body: Center(
+
+      ),
+
+
+    );
+  }
+}
